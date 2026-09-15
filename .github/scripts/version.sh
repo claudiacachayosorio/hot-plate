8d6606bb9c6dd541bcc2961bf8fa80416afce419
claudiacachayosorio <claudiacachayosorio.web@gmail.com>
2026-09-15 16:16:11 -0400

    ci(version): script checks the pr squash merge commit directly, has safeguards against running locally and missing argument

--- .github/scripts/version.sh
+++ .github/scripts/version.sh
@@ -2,7 +2,27 @@
 # =========================================================================== #
 # Description: Automate semantic versioning.
 # =========================================================================== #
+
 set -euo pipefail
+
+if [[ "${GITHUB_ACTIONS}" != "true" ]]; then
+	printf "Script must be run inside a GitHub Actions workflow.\n" >&2
+	exit 1
+fi
+
+if [[ $# -eq 0 ]]; then
+	printf "Commit message is missing.\n" >&2
+	exit 1
+fi
+
+user_name="github-actions[bot]"
+user_email="41898282+github-actions[bot]@users.noreply.github.com"
+git config --global user.name "$user_name"
+git config --global user.email "$user_email"
+
+commit_msg="$1"
+commit_prefix="${commit_msg%%:*}"
+commit_type="${commit_prefix//\(*\)/}"
 
 cur_tag="$(git describe --tags --abbrev=0 2>/dev/null || printf "0.0.0")"
 cur_ver="${cur_tag#v}"
@@ -10,46 +30,20 @@ cur_ver="${cur_tag#v}"
 major=0
 minor=0
 patch=0
-rev_range="HEAD"
 
 if [[ "$cur_tag" != "0.0.0" ]]; then
 	IFS='.' read -r major minor patch <<< "$cur_ver"
-	rev_range="${cur_tag}..HEAD"
 fi
 
-scope_regexp="(\([a-z0-9-]+\))?"
-
-has_commit_matching() {
-	local _prefix="$1"
-	local _regexp="^${_prefix//@/$scope_regexp}:"
-	local _flags=(
-		--format="%H"
-		--extended-regexp
-		--grep="$_regexp"
-	)
-
-	local _match=""
-	_match="$(git log "${_flags[@]}" "$rev_range")"
-	[[ -n "$_match" ]]
-}
-
-if has_commit_matching "[a-z]+@!"; then
-	major=$((major+1))
-	minor=0
-	patch=0
-elif has_commit_matching "feat@"; then
-	minor=$((minor+1))
-	patch=0
-elif has_commit_matching "fix@"; then
-	patch=$((patch+1))
-else
-	exit 0
-fi
+case "$commit_type" in
+	*!)   major=$((major+1)) ; minor=0 ; patch=0 ;;
+	feat) minor=$((minor+1)) ; patch=0 ;;
+	fix)  patch=$((patch+1)) ;;
+	*)    exit 0
+esac
 
 new_ver="${major}.${minor}.${patch}"
 new_tag="v${new_ver}"
 
-git tag \
-	--annotate "$new_tag" \
-	--message "Release ${new_tag}"
+git tag -a "$new_tag" -m "Release ${new_tag}"
 git push origin "$new_tag"
